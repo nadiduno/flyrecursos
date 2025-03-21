f9c290b3958b56cb5b184571ca6853a557a8394d
nadiduno <nadiduno@gmail.com>
2025-03-20 22:31:35 -0300

    Layout da Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,12 +6,22 @@ import { TopTen } from "../componets/TopTen";
 
 export function HomePage(){
     return(
-        <>
-            <Navibar />
-            <HeaderMain />
-            <TopTen />
-            <Resource />
-            <Footer />
-        </>
+        <div className="min-h-screen flex flex-col">
+            <nav className="h-[5.375rem] md:h-[7.75rem]shadow-md">
+                <Navibar />
+            </nav>
+            <header className="h-[23.5rem] md:h-[31.375rem] bg-white text-black">
+                <HeaderMain />
+            </header>
+            <section className="h-[11.5rem] md:h-[38.25rem] text-primary2">
+                <TopTen />
+            </section>
+            <section className="h-[11.5rem] md:h-[38.25rem] text-primary2">
+                <Resource />
+            </section>
+            <footer className="h-[5.375rem] md:h-[7.75rem]">
+                <Footer />
+            </footer>
+        </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary1: "#004054",
+        primary2: "#00CBFE",
+        secondary: "#D3CFC9",
+        neutral: {                    
+          white: '#ffffff',
+          gray: '#666666',           
+          black: '#000000'           
+        },
+      },
+    },
   },
   plugins: [],
 }
