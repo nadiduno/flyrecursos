46a4e3ebac1e7dc087ab4e4de4cb3ef349b32ad2
Ravelos <86490406+Ravelos@users.noreply.github.com>
2025-03-31 20:24:18 +0000

    Ajuste de css do modal

--- src/componets/LoginModal.tsx
+++ src/componets/LoginModal.tsx
@@ -20,7 +20,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onLogin }) => 
 
   return (
     <div className="fixed inset-0  flex flex-col items-center justify-center bg-[#FFFFFFB2] z-50">
-      <div className="bg-[#004054] p-6 rounded-md w-full max-w-sm">
+      <div className="bg-[#004054] p-6 rounded-t-md w-full max-w-sm">
         <h2 className="mt-[50px] mb-[49px] w-[236px] h-[35px] text-3xl mx-auto text-center font-bold font-roboto ">FAÇA SEU LOGIN</h2>
 
         <label className="w-[50px] h-[21px] text-lg font-normal">
@@ -45,7 +45,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onLogin }) => 
         />
         <p>Ainda não tem conta? <span className="font-bold text-[#00CAFE]">Cadastre-se</span></p>
       </div>
-      <div className="flex justify-center bg-[#FFFFFF] rounded-md w-full p-6 items-center max-w-sm">
+      <div className="flex justify-center bg-[#FFFFFF] rounded-b-md w-full p-6 items-center max-w-sm">
           <button onClick={handleLogin} className="w-[400px] h-[40px]  rounded-[50px] bg-[#00CAFE] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.2)]cking-normal text-center font-bold text-[20px]">
             Entrar
           </button>
