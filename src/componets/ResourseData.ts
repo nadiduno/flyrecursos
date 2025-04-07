029a69ac6f61490a1b5b131300d27e4014825c5e
Thati Vasconcelos <thativasconcelos30@gmail.com>
2025-04-07 00:14:55 +0000

    ajustes select video

--- src/componets/ResourseData.ts
+++ src/componets/ResourseData.ts
@@ -5,7 +5,8 @@ export const cardVideos: CardVideoType[] = [
     id: 1,
     title: 'Video 1',
     imageUrl: 'src/assets/imgprevvideo.png',
-    videoUrl: 'https://www.youtube.com/watch?v=DE488HhysSU',
+    //videoUrl: 'https://www.youtube.com/watch?v=DE488HhysSU',
+    videoUrl: 'https://www.youtube.com/embed/DE488HhysSU?si=Wvhfh2xVAI810J2B',
     teacher: 'Professora 1',
     trilha: 'Front-end',
     viewcount: 0
@@ -14,7 +15,7 @@ export const cardVideos: CardVideoType[] = [
     id: 2,
     title: 'Video 2',
     imageUrl: 'src/assets/imgprevvideo.png',
-    videoUrl: 'https://www.youtube.com/watch?v=VR5SNLu6fJI',
+    videoUrl: 'https://www.youtube.com/embed/VR5SNLu6fJI?si=-CACad1V6RXYlHyz',
     teacher: 'Professora 2',
     trilha: 'Front-end',
     viewcount: 0
@@ -23,7 +24,7 @@ export const cardVideos: CardVideoType[] = [
     id: 3,
     title: 'Video 3',
     imageUrl: 'src/assets/imgprevvideo.png',
-    videoUrl: 'https://www.youtube.com/watch?v=DE488HhysSU',
+    videoUrl: 'https://www.youtube.com/embed/DE488HhysSU?si=Wvhfh2xVAI810J2B',
     teacher: 'Professora 3',
     trilha: 'Front-end',
     viewcount: 0
@@ -32,7 +33,7 @@ export const cardVideos: CardVideoType[] = [
     id: 4,
     title: 'Video 4',
     imageUrl: 'src/assets/imgprevvideo.png',
-    videoUrl: 'https://www.youtube.com/watch?v=VR5SNLu6fJI',
+    videoUrl: 'https://www.youtube.com/embed/VR5SNLu6fJI?si=-CACad1V6RXYlHyz',
     teacher: 'Professora 4',
     trilha: 'Front-end',
     viewcount: 0
@@ -41,7 +42,7 @@ export const cardVideos: CardVideoType[] = [
     id: 5,
     title: 'Video 5',
     imageUrl: 'src/assets/imgprevvideo.png',
-    videoUrl: 'https://www.youtube.com/watch?v=DE488HhysSU',
+    videoUrl: 'https://www.youtube.com/embed/DE488HhysSU?si=Wvhfh2xVAI810J2B',
     teacher: 'Professora 5',
     trilha: 'Front-end',
     viewcount: 0
@@ -50,7 +51,7 @@ export const cardVideos: CardVideoType[] = [
     id: 6,
     title: 'Video 6',
     imageUrl: 'src/assets/imgprevvideo.png',
-    videoUrl: 'https://www.youtube.com/watch?v=VR5SNLu6fJI',
+    videoUrl: 'https://www.youtube.com/embed/VR5SNLu6fJI?si=-CACad1V6RXYlHyz',
     teacher: 'Professora 6',
     trilha: 'Front-end',
     viewcount: 0
