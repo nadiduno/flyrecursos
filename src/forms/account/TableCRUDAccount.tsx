fd136d489d6e2ad1e88a5fd33988fa4ad6e35406
nadiduno <nadiduno@gmail.com>
2025-06-27 19:15:40 -0300

    Aplicação do Algoritmo de Módulo 11 - Dígito Verificador para verificar CPF válido

--- src/forms/account/TableCRUDAccount.tsx
+++ src/forms/account/TableCRUDAccount.tsx
@@ -40,11 +40,24 @@ export function TableCRUDAccount({
   };
 
   if (loading) {
-    return <div className="text-center text-yellow">Carregando alunos...</div>;
+    return (
+    <div className="flex flex-col items-center justify-centertext-center text-yellow py-5">
+       <div className="w-12 h-12 border-4 border-secondary border-t-transparent rounded-full animate-spin"></div>
+        <span className="text-white pt-2">Preparando o conteúdo pra você...</span>
+    </div>
+    );
   }
 
   if (error) {
-    return <div className="text-center text-red-500">{error}</div>;
+    return( 
+      <div className="flex flex-col items-center justify-centertext-center text-center text-red-500 py-5">
+      <div className="text-lg font-semibold">
+          Opa! Não conseguimos carregar os alunos no momento.
+          <br />
+          Tente novamente mais tarde.
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -61,10 +74,10 @@ export function TableCRUDAccount({
               Nome Completo
             </th>
             <th scope="col" className="py-3 hidden md:table-cell lg:table-cell">
-              E-mail
+              CPF
             </th>
             <th scope="col" className="py-3 hidden md:table-cell lg:table-cell">
-              CPF
+              E-mail
             </th>
             <th scope="col" className="py-3 hidden md:table-cell lg:table-cell">
               Data de Nasc.
@@ -74,7 +87,7 @@ export function TableCRUDAccount({
             </th>
           </tr>
         </thead>
-        <tbody className="divide-y border-b divide-secondary md:text-sm lg:text-sm">
+        <tbody className="divide-y border-b divide-secondary md:text-s lg:text-s">
           {students.length === 0 ? (
             <tr>
               <td colSpan={3} className="text-center py-4 text-gray-500">
@@ -85,8 +98,8 @@ export function TableCRUDAccount({
             students.map((row) => (
               <tr key={row.id}>
                 <td className="whitespace-nowrap py-2">{row.nome}</td>
-                <td className="whitespace-nowrap py-2 hidden md:table-cell lg:table-cell">{row.email}</td>
                 <td className="whitespace-nowrap py-2 hidden md:table-cell lg:table-cell">{row.cpf}</td>
+                <td className="whitespace-nowrap py-2 hidden md:table-cell lg:table-cell">{row.email}</td>
                 <td className="whitespace-nowrap py-2 hidden md:table-cell lg:table-cell">{formatarData(row.dataNascimento)}</td>
                 <td className="whitespace-nowrap text-right flex flex-row gap-4 py-2 px-2 justify-end">
                   <button onClick={() => onEdit(row)}>
