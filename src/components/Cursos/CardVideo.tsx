740df58b59daaf036fd0bbd00f9ccbe93fd737e7
daysibel1175 <estergutierres1175@gmail.com>
2025-07-13 12:59:15 -0300

    adicionando funcion de getvideopreview

--- src/components/Cursos/CardVideo.tsx
+++ src/components/Cursos/CardVideo.tsx
@@ -15,17 +15,19 @@ export interface CardVideoType{
 interface CardVideoProps {
     cardVideo: CardVideoType
   }
-
+const getVideoPreview = (link: string) => {
+  const match = link.match(/youtube\.com\/embed\/([a-zA-Z0-9_-]+)/);
+  return match ? `https://img.youtube.com/vi/${match[1]}/hqdefault.jpg` : imgDefault;
+};
 export function CardVideo({ cardVideo }: CardVideoProps){
     return(
         <div className="flex-shrink-0">
-            <img
-                src={cardVideo.imagemCapa || imgDefault}
-                alt={`Imagen - ${cardVideo.titulo}`}
-                title={cardVideo.titulo}
-                className="min-w-[147px] max-w-[350px] md:w-[264px] w-[200px] max-h-[150px] md:h-[150px] h-[114px] object-cover bg-lightgreen m-[5px] mx-[10px] cursor-pointer transition-transform duration-500 hover:scale-[1.4] rounded-xl"
-            />
-            
+           <img
+  src={getVideoPreview(cardVideo.linkConteudo)}
+  alt={`Imagen - ${cardVideo.titulo}`}
+  title={cardVideo.titulo}
+  className="min-w-[147px] max-w-[350px] md:w-[264px] w-[200px] max-h-[150px] md:h-[150px] h-[114px] object-cover bg-lightgreen m-[5px] mx-[10px] cursor-pointer transition-transform duration-500 hover:scale-[1.4] rounded-xl"
+/>
         </div>
     )
 }
