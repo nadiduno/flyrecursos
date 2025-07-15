0569b3f4e543ca418b913818dbf3c2704a7ec72a
nadiduno <nadiduno@gmail.com>
2025-07-15 19:22:21 -0300

    Agregando archivo para interfaces

---
+++ src/types/interfaceApi.d.ts
@@ -0,0 +1,36 @@
+export interface UserProfile {
+  id: number;
+  nome: string;
+  email: string;
+  fotoPerfilUrl: string;
+  cursoIds: number[];
+}
+
+export interface Curso {
+  id: number;
+  titulo: string;
+  dataPublicacao: string;
+  autor: {
+    id: number;
+    nome: string;
+    email: string;
+  };
+  modulos: Modulo[];
+}
+
+export interface Modulo {
+  id: number;
+  titulo: string;
+  ordem: number;
+}
+
+export interface Aula {
+  id: number;
+  titulo: string;
+  tipo: string;
+  ordem: number;
+  duracaoEstimada: number;
+  linkConteudo: string;
+  moduloId: number;
+  urlCapa: string;
+}
