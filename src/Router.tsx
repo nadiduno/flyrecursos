9e26914a156b02024a45c9901ad4af50bd86ea00
nadiduno <nadiduno@gmail.com>
2025-07-15 19:18:47 -0300

    Aplicando Lazy Loading as rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,23 +1,28 @@
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import { LessonsPage } from "./pages/LessonsPage";
-import { DashboardAdmin } from "./pages/DashboardAdmin";
+
+// Importações dinâmicas para lazy loading
+const Login = React.lazy(() => import("./pages/Login"));
+const LessonsPage = React.lazy(() => import("./pages/LessonsPage"));
+const DashboardAdmin = React.lazy(() => import("./pages/DashboardAdmin"));
+const Unauthorized = React.lazy(() => import("./pages/Unauthorized"));
 
 import { ProtectedRoute } from "./ProtectedRoute";
-import { Unauthorized } from "./pages/Unauthorized ";
 
 export function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route element={<ProtectedRoute allowedRoles={["ADMIN"]} />}>
-        <Route path="/dashboard" element={<DashboardAdmin />} />
-      </Route>
-       <Route element={<ProtectedRoute allowedRoles={["ADMIN", "ALUNO"]} />}>
-        <Route path="/aulas" element={<LessonsPage />} />
-      </Route>
-      <Route path="/naoautorizado" element={<Unauthorized  />} />
-    </Routes>
+    <Suspense fallback={<div className="w-12 h-12 border-4 border-secondary border-t-transparent rounded-full animate-spin"></div>}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute allowedRoles={["ADMIN"]} />}>
+          <Route path="/dashboard" element={<DashboardAdmin />} />
+        </Route>
+        <Route element={<ProtectedRoute allowedRoles={["ADMIN", "ALUNO"]} />}>
+          <Route path="/aulas" element={<LessonsPage />} />
+        </Route>
+        <Route path="/naoautorizado" element={<Unauthorized />} />
+      </Routes>
+    </Suspense>
   );
 }
