596a04ad99db42e870a816175b2611273813a591
nadiduno <nadiduno@gmail.com>
2025-07-15 19:19:56 -0300

    Configurando o bundle e instalando rollup para o build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true, 
+      filename: 'dist/bundle-analysis.html', 
+    }),
+  ],
+  build: {
+    sourcemap: true, 
+  },
   server: {
   port: 3000,
   }
